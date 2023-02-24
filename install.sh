3b079e981734b3d5784d08aa9c388aaa123d60d1
0x7aa7 <0x7aa7@0x7aa7deMacBook-Pro.local>
2023-02-24 22:00:54 +0800

    update

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 apt install nodejs
 apt install npm
 npm install pm2 -g
+npm ci
--- trader_bot_starter.js
+++ trader_bot_starter.js
@@ -2,6 +2,8 @@ const pm2 = require("pm2");
 const process = require("process");
 const CryptoJS = require("crypto-js");
 const crypto = require("crypto");
+const path = require("path");
+const currentFilePath = path.resolve(__dirname);
 
 var arguments = process.argv.splice(2);
 
@@ -14,29 +16,32 @@ pm2.connect(function (err) {
   }
 
   const func = action && pm2[action];
+  console.log(func, "func");
   if (func) {
     if (action === "start") {
-      try {
-        let trader_bot_args_transformed = JSON.parse(
-          "{" + trader_bot_args.split("b{")[1]
-        );
-        // TODO:参数处理
-        console.log(trader_bot_args_transformed, "trader_bot_args_transformed");
-      } catch (e) {
-        console.log(e);
-      }
+      // try {
+      //   let trader_bot_args_transformed = JSON.parse(
+      //     "{" + trader_bot_args.split("b{")[1]
+      //   );
+      //   // TODO:参数处理
+      //   console.log(trader_bot_args_transformed, "trader_bot_args_transformed");
+      // } catch (e) {
+      //   console.log(e);
+      // }
 
       pm2.start(
         {
-          script: "../HFT",
+          script: path.resolve(currentFilePath, "../HFT"),
           name: process_name,
           //TODO: 参数执行确定
-          args: "",
+          // args: "",
         },
         function (err) {
           if (!err) {
             console.log("start success");
             //TODO: 成功后【开启】状态更新，如进程状态，账号余额/收益情况
+          } else {
+            console.error(err);
           }
         }
       );
@@ -46,6 +51,8 @@ pm2.connect(function (err) {
           console.log("stop success");
           //TODO: 成功后【关闭】 状态更新，如进程状态，账号余额/收益情况
           pm2.stop(process_name);
+        } else {
+          console.error(err);
         }
       });
     }
