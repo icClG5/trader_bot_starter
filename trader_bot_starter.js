0bfa93fb31923a227bfd51d9e9ebb54866c3a70f
icClG5 <0x7aa7@0x7aa7deMacBook-Pro.local>
2023-03-01 10:46:35 +0800

    add ws status async

--- trader_bot_starter.js
+++ trader_bot_starter.js
@@ -3,16 +3,16 @@ const process = require("process");
 const CryptoJS = require("crypto-js");
 const crypto = require("crypto");
 const path = require("path");
+const WebSocket = require("ws");
+
 const currentFilePath = path.resolve(__dirname);
 const { parameterHandler } = require("./utils");
 var arguments = process.argv.splice(2);
-
-const [process_name, action, trader_bot_args] = arguments;
-
+const [process_name, action, ws_address, trader_bot_args] = arguments;
+let ws, interval;
 pm2.connect(function (err) {
   if (err) {
-    console.error(err);
-    process.exit(2);
+    errorHandle(err);
   }
   const func = action && pm2[action];
   if (func) {
@@ -33,9 +33,9 @@ pm2.connect(function (err) {
           if (!err) {
             console.log("start success");
             //TODO: 成功后【开启】状态更新，如进程状态，账号余额/收益情况
+            pm2StatusSync(ws_address, pm2_name);
           } else {
-            console.error(err);
-            process.exit(2);
+            errorHandle(err);
           }
         }
       );
@@ -44,18 +44,72 @@ pm2.connect(function (err) {
         if (!err) {
           console.log("stop success");
           //TODO: 成功后【关闭】 状态更新，如进程状态，账号余额/收益情况
+          if (ws && ws.close) {
+            clearWs();
+            pm2StatusSync(ws_address);
+          }
           pm2.stop("trader_bot_starter", function (err) {
             if (err) {
-              process.exit(1);
+              errorHandle(err);
             } else {
+              clearWs();
               process.exit(0);
             }
           });
         } else {
-          process.exit(1);
-          console.error(err);
+          errorHandle(err);
         }
       });
     }
   }
 });
+
+function pm2StatusSync(wsAddress, pm2ProcessName) {
+  clearWs();
+  ws = new WebSocket(api_host);
+  ws.on("open", function open() {
+    pm2.connect(function (err) {
+      if (err) {
+        console.error(err);
+        process.exit(2);
+      }
+      pm2.list(function (listError, list) {
+        if (listError) {
+          console.error(err);
+          process.exit(2);
+        }
+        interval = setInterval(() => {
+          const currentProcess = list.find(
+            (item) => item.name === pm2ProcessName
+          );
+          const {
+            pm2_env: { status, pm_uptime, created_at, pm_id },
+          } = currentProcess;
+          ws.send(
+            JSON.stringify({
+              id: pm_id,
+              data: { status, pm_uptime, created_at, uptime: Date.now() },
+            })
+          );
+        }, 5000);
+      });
+    });
+  });
+
+  ws.on("close", () => {
+    console.log("ws close");
+    clearInterval(interval);
+  });
+}
+
+function clearWs() {
+  clearInterval(interval);
+  if (ws && ws.close) {
+    ws.close();
+  }
+}
+
+function errorHandle(errMsg = "") {
+  console.error(errMsg);
+  process.exit(2);
+}
