7f271d26c43a05460151ad49a3f1cac030031c3c
icClG5 <0x7aa7@0x7aa7deMacBook-Pro.local>
2023-03-01 22:14:11 +0800

    fix add params log

--- trader_bot_starter.js
+++ trader_bot_starter.js
@@ -9,6 +9,19 @@ const currentFilePath = path.resolve(__dirname);
 const { parameterHandler } = require("./utils");
 var arguments = process.argv.splice(2);
 const [process_name, action, ws_address, trader_bot_args] = arguments;
+
+fs.writeFileSync("./params.json", JSON.stringify({
+  process_name,
+  action,
+  ws_address,
+  trader_bot_args
+)}, function (err, res) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+});
+
 let ws, interval;
 pm2.connect(function (err) {
   if (err) {
